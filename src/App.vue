0c328db8542538119992c7265d66d461ac2ed12e
Lint Action <lint-action@samuelmeuli.com>
2022-01-11 01:36:14 +0000

    style: fix issues with Prettier

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
 	<div id="nav">
 		<router-link to="/">Home</router-link> |
 		<router-link to="/products">Our Products</router-link> |
-		<router-link to="/products/devils-food-cake">Products Detail Test</router-link> |
+		<router-link to="/products/devils-food-cake"
+			>Products Detail Test</router-link
+		>
+		|
 		<router-link to="/cart">Your Cart</router-link>
 	</div>
 	<router-view />
