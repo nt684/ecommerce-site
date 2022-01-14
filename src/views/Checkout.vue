e9fd57588c49404e8d47fc84ca250691ab89a26b
Silversought <lord.bunnywinks@gmail.com>
2022-01-13 21:11:36 -0800

    feat: finished card details, added checkout->delivery

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -11,13 +11,38 @@
 				<div class="field-body">
 					<div class="field">
 						<div class="control">
-							<input
-								class="input"
-								type="text"
-								placeholder="xxxx xxxx xxxx xxxx"
-							/>
+							<input class="input"
+								   type="text"
+								   placeholder="xxxx xxxx xxxx xxxx" />
 						</div>
 					</div>
+				</div>
+				<div class="field-label is-normal">
+					<label class="label">Expiration Date</label>
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control">
+							<input class="input"
+								   type="text"
+								   placeholder="mm/yyyy" />
+						</div>
+					</div>
+				</div>
+				<div class="field-label is-normal">
+					<label class="label">Security Code</label>
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control">
+							<input class="input"
+								   type="text"
+								   placeholder="xxx" />
+						</div>
+					</div>
+				</div>
+				<div class="buttons">
+					<a class="button is-primary" href="/delivery">Confirm Order</a>
 				</div>
 			</div>
 		</section>
